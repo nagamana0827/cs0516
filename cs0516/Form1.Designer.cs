54ba69d62c269c4cfaab51f039ffb19d026c71b8
nagayamamana <nagamiyama1031@gmail.com>
2025-05-16 15:55:51 +0900

    a

--- cs0516/Form1.Designer.cs
+++ cs0516/Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(504, 218);
+            button1.Name = "button1";
+            button1.Size = new Size(123, 101);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(319, 192);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(410, 228);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 80);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(198, 58);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            textBox1.Text = "yuu";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(116, 201);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 4;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private Button button3;
     }
 }
